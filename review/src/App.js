eef068c9c0fc982d3871b2c204c7f48cb7ba2362
Keiran Kozlowski <keirankozlowski@gmail.com>
2021-07-12 13:28:20 -0400

    done with review

--- review/src/App.js
+++ review/src/App.js
@@ -16,12 +16,35 @@ export default function App() {
     dispatch(actions.addAction(1));
   }
 
+  const handleSubtract = () => {
+    dispatch(actions.subtractAction(1));
+  }
+
+  const handleClear = () => {
+    dispatch(actions.clearAction());
+  }
+
+  const handleAddToMemory = () => {
+    dispatch(actions.addToMemoryAction());
+  }
+
+  console.log(state.memory);
+
   return (
     <div className="App">
       <textarea rows="1" value="0" id="total" type="text" name="ans" value={state.currentValue}></textarea>
       <br />
       <button type="button" className="btn" onClick={handleAdd}>
         + 1
+      </button>
+      <button type="button" className="btn" onClick={handleSubtract}>
+        - 1
+      </button>
+      <button type="button" className="btn" onClick={handleClear}>
+        Clear
+      </button>
+      <button type="button" className="btn" onClick={handleAddToMemory}>
+        Add To Memory
       </button>
     </div>
   );
